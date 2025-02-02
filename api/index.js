d6a8cd4d071b2910511ee330224563a8fb78c209
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2025-02-02 17:32:27 +0530

    Ready to deploye Backend in hostinger VPS

--- api/index.js
+++ api/index.js
@@ -69,6 +69,6 @@ process.on('SIGTERM', () => {
 });
 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on Port ${process.env.PORT || 3000}`);
+app.listen(8080, '0.0.0.0', () => {
+  console.log('Server running on port 8080');
 });
