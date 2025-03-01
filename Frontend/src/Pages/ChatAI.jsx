2ece941f5125c2c4505bae6e782957658aaef5fa
SantheepG <gshantheep@gmail.com>
2025-03-01 22:51:03 +0530

    UI changes in ChatAI section

--- Frontend/src/Pages/ChatAI.jsx
+++ Frontend/src/Pages/ChatAI.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
 import logo from "../assets/Logo/newlogo.png";
-import sendIcon from "../assets/Logo/sendlogo.png";
 // import { Search } from "lucide-react";
 import { motion } from "framer-motion";
-import searchIcon from "../assets/Logo/searchIcon.png";
+import searchIcon from "../assets/Logo/circleArrow.png";
 import shareIcon from "../assets/Logo/shareIcon.png";
 import copyIcon from "../assets/Logo/copyIcon.png";
-import { useNavigate } from 'react-router-dom';
 
 const ChatAI = () => {
   const [query, setQuery] = useState("");
   const [answer, setAnswer] = useState("");
   const [loading, setLoading] = useState(false);
   const [shareClicked, setSharedClicked] = useState(false);
-  const navigate = useNavigate();
+  const [copyClicked, setCopyClicked] = useState(false);
 
   useEffect(() => {
     if (shareClicked) {
@@ -23,6 +21,15 @@ const ChatAI = () => {
     }
   },[shareClicked])
 
+  useEffect(() => {
+    if (copyClicked) {
+      setTimeout(() => {
+          setCopyClicked(false);
+        }, 1000);
+    }
+    
+  }, [copyClicked])
+  
   const sendQuery = async () => {
     try {
       if (query !== "") {
@@ -86,8 +93,8 @@ const ChatAI = () => {
 
   return (
     <>
-      <div className="max-h-screen overflow-hidden">
-        <div className="flex item-center text-3xl text-gray-800 bg-gradient-to-r from-sky-200 to-pink-200 px-16 py-8" >
+      <div className="max-h-screen overflow-hidden lg:bg-[url(../assets/bg5.png)] md:bg-[url(../assets/bg-md.png)] bg-[url(../assets/bg-sm.png)] bg-cover bg-center">
+        <div className="flex item-center text-3xl text-gray-800 px-16 py-8" >
           <motion.div
                     className="flex items-center"
                     initial={{ opacity: 0, x: -300 }}
@@ -104,21 +111,28 @@ const ChatAI = () => {
               // }}
             />
           </div>
-          <h1 className="text-3xl  text-gray-800 bg-gradient-to-r from-sky-200 ">
+          <h1 className="text-3xl  text-gray-800 bg-transparent">
             aMusicBible/AI
           </h1>
                 </motion.div>
-          
         </div>
-        <div className="flex flex-col items-center  min-h-screen bg-gradient-to-r from-sky-200 to-pink-200 " >
-          <div className="mr-56 text-gray-500 font-semibold mb-1">
+        <div className="flex justify-start">
+          <div className="lg:w-1/5 mt-36">
+            {/* <img src="../assets/big_3D-setting_image.png" alt="align-image1" className="w-48 justify-self-end relative top-20 sm:left-12 z-[999]" />
+            <img src="../assets/align-image1.png" alt="align-image1" className="absolute lg:w-[500px] -mt-6" /> */}
+          </div>
+          <div className="lg:w-3/5 w-full z-[9999]">
+            <div className=" text-gray-500 mb-6 lg:ml-36 md:ml-36 ml-12">
             Ask your thoughts
           </div>
-          <div className="relative flex ">
+            <div className="flex flex-col items-center min-h-screen" >
+          
+              <div className="flex justify-between">
+                <div className="relative flex ">
             <input
               type="text"
-              className="px-4 py-2 w-80 rounded-full shadow-2xl border border-gray-300 focus:outline-none bg-gradient-to-r from-blue-100 to-pink-200 "
-              placeholder="Type here"
+              className="px-4 py-2 lg:w-[650px] md:w-[460px] w-[280px] lg:-ml-2 h-14 rounded-full shadow-2xl border border-gray-300 focus:outline-none shadow-gray-600 bg-transparent shadow-lg"
+              placeholder="How to be a good Christian?"
               value={query}
               onChange={(e) => setQuery(e.target.value)}
               onKeyDown={(e) => {
@@ -127,19 +141,20 @@ const ChatAI = () => {
                 }
               }}
             />
-            <div className="absolute w-8 h-8 flex justify-end ml-[280px] mt-3">
-              <img src={searchIcon} alt="send" className="w-8 h-8" />
-            </div>
-            <button
+            
+            {/* <button
               className="ml-2 p-1 rounded-full hover:scale-110 transition "
               onClick={sendQuery}
             >
               <div className="w-12 h-12 flex items-center ">
                 <img src={sendIcon} alt="send" className="" />
               </div>
-            </button>
+            </button> */}
+          </div><div className=" w-14 h-14 flex justify-end mt-3 -ml-16 z-[999] cursor-pointer">
+              <img src={searchIcon} alt="send" className="w-14 h-12 -mt-2" onClick={sendQuery}/>
+            </div>
           </div>
-          <div className="flex gap-4 mt-4 -ml-12">
+          <div className="flex gap-4 mt-6 mb-4 -ml-12">
             <a
               href="https://play.google.com/store/apps/details?id=com.faite.project.music_bible_music_player&pcampaignid=web_share
 "
@@ -150,7 +165,7 @@ const ChatAI = () => {
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/1200px-Google_Play_Store_badge_EN.svg.png"
                   alt="Google Play"
-                  className="w-[95px] h-auto mt-0.5"
+                  className="w-[110px] h-auto mt-0.5"
                 />
               </motion.div>
             </a>
@@ -163,36 +178,35 @@ const ChatAI = () => {
                 <img
                   src="https://p7.hiclipart.com/preview/422/842/453/app-store-android-google-play-get-started-now-button.jpg"
                   alt="Apple Store"
-                  className="w-[80px] h-auto"
+                  className="w-[104px] h-auto"
                 />
               </motion.div>
             </a>
           </div>
-
-          <div className=" w-3/4 max-w-md">
+          <div className="w-3/4 lg:max-w-2xl max-w-lg">
             {" "}
-            <p className="text-gray-500 w-full font-semibold ml-4">
+            <p className="text-gray-500 w-full ml-4">
               Answer
             </p>{" "}
-            <div className="mt-2 p-4 bg-white border-t rounded-t-2xl shadow-2xl relative h-screen flex flex-col justify-between bg-gradient-to-r from-blue-100 to-pink-200 ">
+            <div className="mt-2 p-4 bg-white border-t shadow-2xl relative h-screen flex flex-col justify-between shadow-black bg-gray-50  rounded-t-[50px]">
               {loading ? (
-                <div className="type_loader_container">
+                <div className="type_loader_container ml-4">
                   <div className="typing_loader"></div>
                 </div>
               ) : (
-                <div>............</div>
+                <div className="ml-4">............</div>
               )}
               
               <div className="mt-2 text-gray-800 font-medium flex-grow break-words">
-                <div className="  h-[400px] overflow-y-auto text-gray-600">
+                <div className="h-[400px] overflow-y-auto text-gray-600">
                   {answer && answer!=="" && ParseText(answer)}
                 </div>
               </div>
               
-              <div className="absolute top-3 right-3 flex space-x-2">
+              <div className="absolute top-3 lg:right-8 right-4 flex space-x-2">
                 {/* <button className="text-gray-500 hover:text-gray-700">↩</button> */}
                 {
-                  shareClicked && (<div className="text-gray-500 mx-10 animate-view-content">Link copied</div>)
+                  copyClicked && (<div className="text-gray-500 mx-10 animate-view-content">Text copied</div>)
                 }
                 <button
                   className="text-gray-500 hover:text-gray-700 text-2xl hover:scale-110 transition"
@@ -211,7 +225,7 @@ const ChatAI = () => {
                     textArea.select();
                     document.execCommand("copy"); // Copies text
                     document.body.removeChild(textArea);
-                    console.log("Copied to clipboard");
+                    setCopyClicked(true)
                   }}
                 >
                   <div className="w-8 h-8 flex items-center ">
@@ -220,18 +234,17 @@ const ChatAI = () => {
                 </button>
               </div>
             </div>
+          </div>   
+            </div>
+          </div>
+          <div className="lg:w-1/5 mt-36">
+            {/* <img src="../assets/big_3D-setting_image.png" alt="align-image1" className="w-48 justify-self-start relative top-20  z-[999]" /> */}
+            {/* <img src="../assets/align-image2.png" alt="align-image2" className="absolute lg:w-[480px] -ml-36 mt-24" /> */}
           </div>
 
-          {/* <div className="mt-4 flex items-center gap-2 text-gray-700">
-                <p>Feedbacks</p>
-                <div className="flex gap-1">
-                    {[...Array(5)].map((_, index) => (
-                        <span key={index} className="text-yellow-500 text-xl">⭐</span>
-                    ))}
-                </div>
-            </div> */}
         </div>
       </div>
+      
     </>
   );
 };
